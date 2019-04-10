8b7de4ab6856de4774c897b1e59e624faa8c8f9b
szkszk95 <739924940@qq.com>
2019-04-10 10:11:48 +0800

    update cycleGAN

--- cyclegan/trainer.py
+++ cyclegan/trainer.py
@@ -6,12 +6,14 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision.utils as vutils
-from cyclegan.utils.dataset import DATASET
+from utils.dataset import DATASET
 from itertools import chain
+from tensorboardX import SummaryWriter
 
-from cyclegan.Discriminator import Discriminator
-from cyclegan.Generator import Generator
-from cyclegan.dataloader import dataloader
+
+from Discriminator import Discriminator
+from Generator import Generator
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--datasetA', help="images path of domain A",
@@ -26,7 +28,7 @@ parser.add_argument('--imageSize', type=int, default=128, help='the height / wid
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
 parser.add_argument('--nc', type=int, default=3)
-parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=40000, help='number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda', action='store_true', help='enables cuda')
@@ -51,20 +53,22 @@ except OSError:
 
 device = torch.device("cuda:0" if torch.cuda.is_available else "cpu")
 
-
-dataset = dataloader(opt.dataroot, opt.datasetA, opt.datasetB, bs=opt.batchSize, if_generate=False)
-datasetA = DATASET('/home/szk/PycharmProjects/open-reid/examples/data/cuhk03/images', 143, 128, 1)
-datasetB = DATASET('/home/szk/PycharmProjects/open-reid/examples/data/viper/images', 143, 128, 1)
+# dataset = dataloader(opt.dataroot, opt.datasetA, opt.datasetB, bs=opt.batchSize, if_generate=False)
+# datasetA = DATASET('/home/szk/PycharmProjects/open-reid/examples/data/cuhk03/images', 143, 128, 1)
+# datasetB = DATASET('/home/szk/PycharmProjects/open-reid/examples/data/viper/images', 143, 128, 1)
+datasetA = DATASET('/home/szk/PycharmProjects/pytorch-CycleGAN-and-pix2pix/datasets/horse2zebra/trainA', 143, 128, 1)
+datasetB = DATASET('/home/szk/PycharmProjects/pytorch-CycleGAN-and-pix2pix/datasets/horse2zebra/trainB', 143, 128, 1)
 loader_A = torch.utils.data.DataLoader(dataset=datasetA,
                                        batch_size=opt.batchSize,
-                                       shuffle=True,
-                                       num_workers=2)
+                                       shuffle=True)
 loaderA = iter(loader_A)
 loader_B = torch.utils.data.DataLoader(dataset=datasetB,
                                        batch_size=opt.batchSize,
-                                       shuffle=True,
-                                       num_workers=2)
+                                       shuffle=True)
 loaderB = iter(loader_B)
+
+writer = SummaryWriter()
+
 
 # custom weights initialization called on netG and netD
 def weights_init(m):
@@ -100,85 +104,87 @@ optimizerG = optim.Adam(chain(netGAB.parameters(), netGBA.parameters()), lr=0.00
 label_real = 1
 label_fake = 0
 
-for epoch in range(opt.niter):
-    for i in range(dataset.iters()):
-        # imageA, imageB = dataset.next()
-        # imageA, imageB = torch.Tensor(imageA).to(device), torch.Tensor(imageB).to(device)
-        loaderA, loaderB = iter(loader_A), iter(loader_B)
-        imageA = loaderA.next().to(device)
-        imageB = loaderB.next().to(device)
+for niter in range(opt.niter):
+    # imageA, imageB = dataset.next()
+    # imageA, imageB = torch.Tensor(imageA).to(device), torch.Tensor(imageB).to(device)
+    loaderA, loaderB = iter(loader_A), iter(loader_B)
+    imageA = loaderA.next().to(device)
+    imageB = loaderB.next().to(device)
 
-        # --------train fDx---------
-        # train DA,DB with real image
-        netDA.zero_grad()
-        netDB.zero_grad()
+    # --------train fDx---------
+    # train DA,DB with real image
+    netDA.zero_grad()
+    netDB.zero_grad()
 
-        out_real_A = netDA.forward(imageA)
-        out_real_B = netDA.forward(imageB)
-        label_r = torch.full(out_real_A.shape, label_real, device=device)
+    out_real_A = netDA.forward(imageA)
+    out_real_B = netDA.forward(imageB)
+    label_r = torch.full(out_real_A.shape, label_real, device=device)
 
-        errDA_real = BCELoss(out_real_A, label_r)
-        errDB_real = BCELoss(out_real_B, label_r)
+    errDA_real = BCELoss(out_real_A, label_r)
+    errDB_real = BCELoss(out_real_B, label_r)
+    err_real = errDA_real + errDB_real
+    # err_real.backward()
 
-        err_real = errDA_real + errDB_real
-        err_real.backward()
+    # train DA,DB with fake image
+    fake_AB = netGAB(imageA)
+    fake_BA = netGBA(imageB)
 
-        # train DA,DB with fake image
-        fake_AB = netGAB(imageA)
-        fake_BA = netGBA(imageB)
+    out_fake_A = netDB.forward(fake_AB.detach())
+    out_fake_B = netDA.forward(fake_BA.detach())
+    label_f = torch.full(out_fake_A.shape, label_fake, device=device)
 
-        out_fake_A = netDB.forward(fake_AB.detach())
-        out_fake_B = netDA.forward(fake_BA.detach())
-        label_f = torch.full(out_fake_A.shape, label_fake, device=device)
+    errDA_fake = BCELoss(out_fake_A, label_f)
+    errDB_fake = BCELoss(out_fake_B, label_f)
+    err_fake = errDA_fake + errDB_fake
+    # err_fake.backward()
 
-        errDA_fake = BCELoss(out_fake_A, label_f)
-        errDB_fake = BCELoss(out_fake_B, label_f)
-        err_fake = errDA_fake + errDB_fake
-        err_fake.backward()
+    errD = (err_real + err_fake) * 0.5
+    errD.backward()
 
-        optimizerDA.step()
-        optimizerDB.step()
+    optimizerDA.step()
+    optimizerDB.step()
 
-        # --------train fGx---------
-        # train GBA with fake image, L1loss between fake and real image
-        for j in range(5):
-            netGAB.zero_grad()
-            netGBA.zero_grad()
+    # --------train fGx---------
+    # train GBA with fake image, L1loss between fake and real image
+    netGAB.zero_grad()
+    netGBA.zero_grad()
 
-            fake_AB = netGAB(imageA)
-            fake_BA = netGBA(imageB)
+    fake_AB = netGAB(imageA)
+    fake_ABA = netGBA(fake_AB)
 
-            fake_ABA = netGBA(fake_AB)
-            fake_BAB = netGAB(fake_BA)
-            out_fake_ABA = netDA(fake_ABA)
-            out_fake_BAB = netDB(fake_BAB)
+    fake_BA = netGBA(imageB)
+    fake_BAB = netGAB(fake_BA)
 
-            # GAN LOSS
-            err_ABA = BCELoss(out_fake_ABA, label_r)
-            err_BAB = BCELoss(out_fake_BAB, label_r)
-            err_GAN = err_ABA+err_BAB
+    out_fake_ABA = netDA(fake_ABA)
+    out_fake_BAB = netDB(fake_BAB)
 
-            # MSE LOSS
-            dis_A = L1Loss(fake_ABA, imageA)
-            dis_B = L1Loss(fake_BAB, imageB)
-            err_MSE = dis_A + dis_B
+    # GAN LOSS
+    err_ABA = BCELoss(out_fake_ABA, label_f)
+    err_BAB = BCELoss(out_fake_BAB, label_f)
+    err_GAN = err_ABA + err_BAB
 
-            err_G = err_GAN+err_MSE
-            err_G.backward()
+    # MSE LOSS
+    dis_A = L1Loss(fake_ABA, imageA)
+    dis_B = L1Loss(fake_BAB, imageB)
+    err_MSE = (dis_A + dis_B) * 10.0
 
-            optimizerG.step()
+    err_G = err_GAN + err_MSE
+    err_G.backward()
 
-        print('[{}/{}][{}/{}] netD_real {:.4f} netD_fake {:.4f} netG {:.4f}'.format(
-            epoch,
-            opt.niter,
-            i,
-            dataset.iters(),
-            err_fake.data[0], err_real.data[0], err_G.data[0]))
+    optimizerG.step()
 
-        if i % 10 == 0:
-            vutils.save_image(imageA, '{}/epoch_{}_real_A.png'.format(opt.outf, epoch), normalize=True)
-            vutils.save_image(imageB, '{}/epoch_{}_real_B.png'.format(opt.outf, epoch), normalize=True)
-            vutils.save_image(fake_AB, '{}/epoch_{}_fake_A2B.png'.format(opt.outf, epoch), normalize=True)
-            vutils.save_image(fake_BA, '{}/epoch_{}_fake_B2A.png'.format(opt.outf, epoch), normalize=True)
-            vutils.save_image(fake_ABA, '{}/epoch_{}_fake_A2B2A.png'.format(opt.outf, epoch), normalize=True)
-            vutils.save_image(fake_BAB, '{}/epoch_{}_fake_B2A2B.png'.format(opt.outf, epoch), normalize=True)
+    # tensorboardX
+    writer.add_scalar('netD_real', err_real.data[0], niter)
+
+    print('[{}/{}] netD_real {:.4f} netD_fake {:.4f} netG_GAN {:.4f} netG_MSE {:.4f}'.format(
+        niter,
+        opt.niter,
+        err_fake.data[0], err_real.data[0], err_GAN.data[0], err_MSE.data[0]))
+
+    if niter % 10 == 0:
+        vutils.save_image(imageA, '{}/epoch_{}_A_real.png'.format(opt.outf, 500), normalize=True)
+        vutils.save_image(imageB, '{}/epoch_{}_B_real.png'.format(opt.outf, 500), normalize=True)
+        vutils.save_image(fake_AB, '{}/epoch_{}_A2B_fake.png'.format(opt.outf, 500), normalize=True)
+        vutils.save_image(fake_BA, '{}/epoch_{}_B2A_fake.png'.format(opt.outf, 500), normalize=True)
+        vutils.save_image(fake_ABA, '{}/epoch_{}_A2B2A_fake.png'.format(opt.outf, 500), normalize=True)
+        vutils.save_image(fake_BAB, '{}/epoch_{}_B2A2B_fake.png'.format(opt.outf, 500), normalize=True)
